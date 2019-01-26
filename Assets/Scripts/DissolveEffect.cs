5cf620ffb68bc1c406aa7a88ef92950383dc2080
ofgonen <ofgonen@gmail.com>
2019-01-25 16:29:45 -0800

    DissolveShader Added
    Dissolve Shader added for , fade in and fade out animation

---
+++ Assets/Scripts/DissolveEffect.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DissolveEffect : MonoBehaviour
+{
+
+    public Material _mat;
+
+    [Range(0, 1)]
+    public float _effect_value;
+
+
+    private void OnRenderImage(RenderTexture source, RenderTexture destination)
+    {
+        Graphics.Blit(source, destination, _mat);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _mat.SetFloat("_NoiseThreshold", _effect_value);
+
+    }
+}
