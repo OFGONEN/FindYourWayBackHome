ffa5be9ed6c6da2918b2fc2ed87d631df009ad9f
ofgonen <ofgonen@gmail.com>
2019-01-25 17:04:36 -0800

    MainMenuPrototip and UIManager Script Added
    Place holder UI elements added but UIManager Scripts is valid. UIManager handles opening About and Credits UI Objects.

---
+++ Assets/Scripts/UIManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class UIManager : MonoBehaviour
+{
+
+    public GameObject _main;
+    public GameObject _about;
+    public GameObject _credits;
+
+    public void OnActiveAbout()
+    {
+        _about.active = true;
+        _main.active = false;
+    }
+
+    public void OnActiveCredits()
+    {
+        _credits.active = true;
+        _main.active = false;
+    }
+
+
+    public void OnDisable()
+    {
+        _credits.active = false;
+        _about.active = false;
+
+        _main.active = true;
+    }
+}
