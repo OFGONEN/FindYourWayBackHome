20401710ece3587bc5103f26ac182fce6915298d
ofgonen <ofgonen@gmail.com>
2019-01-27 01:44:38 -0800

    MainMenu About Added. FadeIn Call Changed
    If you want to call FadeIn on Scene start you have to make _fadein_onstart = true.

--- Assets/Scripts/OnSceneLoad.cs
+++ Assets/Scripts/OnSceneLoad.cs
@@ -4,45 +4,48 @@ using UnityEngine;
 
 public class OnSceneLoad : MonoBehaviour
 {
-	public static OnSceneLoad instance;
+    public static OnSceneLoad instance;
+    public bool _fadein_onstart;
 
-	ProbLoader _probLoader;
+    ProbLoader _probLoader;
 
-	public bool _loaded_room1;
-	public bool _loaded_room2;
+    public bool _loaded_room1;
+    public bool _loaded_room2;
 
-	private void Awake()
-	{
-		if(instance == null )
-		{
-			instance = this;
-		}
-		else if(instance != this )
-		{
-			Destroy( gameObject );
-		}
-
-		_probLoader = GetComponent<ProbLoader>();
-	}
-
-	void Start()
+    private void Awake()
     {
-        DissolveEffect.instance.FindCamera();
-        DissolveEffect.instance.FadeIn();
+        if (instance == null)
+        {
+            instance = this;
+        }
+        else if (instance != this)
+        {
+            Destroy(gameObject);
+        }
+
+        _probLoader = GetComponent<ProbLoader>();
     }
 
-	public void SceneLoaded(int number )
-	{
-		if( number == 1 )
-			_loaded_room1 = true;
-		else
-			_loaded_room2 = true;
+    void Start()
+    {
+        DissolveEffect.instance.FindCamera();
+        if (_fadein_onstart)
+            DissolveEffect.instance.FadeIn();
+    }
 
-		if(_loaded_room1 && _loaded_room2 )
-		{
-			_probLoader.AddRooms();
-		}
-	}
+    public void SceneLoaded(int number)
+    {
+        if (number == 1)
+            _loaded_room1 = true;
+        else
+            _loaded_room2 = true;
+
+        if (_loaded_room1 && _loaded_room2)
+        {
+            _probLoader.AddRooms();
+            DissolveEffect.instance.FadeIn();
+        }
+    }
 
 
 
